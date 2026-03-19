b8bac713c459a024ec8791741da96d12003083ea
deCipher <decipher@kali.kali>
2026-03-20 02:40:09 +0300

    learn path parameters

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": {"name": "Gabby Jablo"}}
+    return {"data": "Blog list"}
 
-@app.get("/about")
-def about():
-    return {"data": {"age": 30, "hobbies": ["coding", "traveling", "cooking"]}}
+@app.get("/blog/{blog_id}")
+def get_blog(blog_id: int):
+    return {"data": {"id": blog_id, "title": f"Blog {blog_id}", "content": "This is the content of the blog post."}}
+
+@app.get("/blog/{blog_id}/comments")
+def get_comments(blog_id: int):
+    return {"data": [{"id": 1, "blog_id": blog_id, "content": "Great post!"}, {"id": 2, "blog_id": blog_id, "content": "Thanks for sharing!"}]}
 
 
